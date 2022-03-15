537b10a2025ffafe5eb6b61f89c010b606dc567e
Mugen <paradigmcomplex@gmail.com>
2022-03-15 14:13:17 -0500

    champloo gifs

--- src/App.js
+++ src/App.js
@@ -1,98 +1,162 @@
-import React, {useEffect, useState} from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
 
 // Constants
-const TWITTER_HANDLE = 'mugen_newjabes';
+const TWITTER_HANDLE = "mugen_newjabes";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+//Test Gifs
+const TEST_GIFS = [
+  "https://c.tenor.com/dSPOmUJ6_t4AAAAC/mugen-samurai.gif",
+  "https://c.tenor.com/W5FAFi8mD7YAAAAC/anime-yawn.gif",
+  "https://c.tenor.com/M4wI-gM7UtYAAAAC/mugen-smiling.gif",
+  "https://c.tenor.com/KKIU6ZE6s4EAAAAC/mugen-pt.gif",
+  "https://c.tenor.com/8q4KXNXoBYUAAAAC/mugen-z2.gif",
+];
+
 const App = () => {
-    // State
-    const [walletAddress, setWalletAddress] = useState(null);
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [gifList, setGifList] = useState([]);
 
-    /**
-     * This function holds the logic for deciding if a Phantom wallet is
-     * connectec or not
-     */
-    const checkIfWalletIsConnected = async () => {
-        try {
-            const { solana } = window;
+  /**
+   * This function holds the logic for deciding if a Phantom wallet is
+   * connectec or not
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
 
-            if (solana) {
-                if (solana.isPhantom) {
-                    console.log('Phantom wallet found!');
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
 
-                    /**
-                     * The solana object gives us a function that will allow us to connect
-                     * directly with the user's wallet!
-                     */
-                    const response = await solana.connect({ onlyIfTrusted: true});
-                    console.log(
-                        'Connected with Public Key:',
-                        response.publicKey.toString()
-                    );
+          /**
+           * The solana object gives us a function that will allow us to connect
+           * directly with the user's wallet!
+           */
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString()
+          );
 
-                    /**
-                     * Set the user's publicKey in state to used later!
-                     */
-                    setWalletAddress(response.publicKey.toString());
-                }
-            } else {
-                alert('Solana object not found! Get a Phantom Wallet 👻');
-            }
-        } catch (error) {
-            console.error(error);
+          /**
+           * Set the user's publicKey in state to used later!
+           */
+          setWalletAddress(response.publicKey.toString());
         }
-    };
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    /**
-     * Let's define this method so our code doesn't break.
-     * We will write the logic for this next!
-     */
-    const connectWallet = async () => {
-        const { solana } = window;
+  /**
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+   */
+  const connectWallet = async () => {
+    const { solana } = window;
 
-        if (solana) {
-            const response = await solana.connect();
-            console.log('Connected with Public Key:', response.publicKey.toString());
-            setWalletAddress(response.publicKey.toString());
-        }
-    };
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
 
-    /**
-     * We want to render this UI when the user hasn't connected
-     * their wallet to our app yet
-     */
-    const renderNotConnectedContainer = () => (
-        <button
-            className="cta-button connect-wallet-button"
-            onClick={connectWallet}
-        >
-            Connect to Wallet
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log("Gif link:", inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue("");
+    } else {
+      console.log("Empty input. Try again.");
+    }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  /**
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet
+   */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div clasName="connected-container">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input
+          type="text"
+          palceholder="Enter Samurai Champloo GIF link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="cta-button submit-gif-button">
+          Submit
         </button>
-    );
+      </form>
+      <div className="gif-grid">
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
-    /**
-     * When our component first mounts, let's check to see if we have a connected Phantom Wallet
-     */
-    useEffect(() => {
-        const onLoad = async () => {
-            await checkIfWalletIsConnected();
-        };
-        window.addEventListener('load', onLoad);
-        return () => window.removeEventListener('load', onLoad);
-    }, []);
+  /**
+   * When our component first mounts, let's check to see if we have a connected Phantom Wallet
+   */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log("Fetching GIF list...");
+
+      //Call Solana program here.
+
+      //Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
 
   return (
     <div className="App">
-      <div className={walletAddress ? 'authed-container' : 'container'}>
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
-          </p>
+          <p className="header">Samurai Champloo GIF Sekai</p>
+          <p className="sub-text">Only Samurai Champloo GIFs ✨</p>
           {/* Render your connect to wallet button right here */}
           {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
